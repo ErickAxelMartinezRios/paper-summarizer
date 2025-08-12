260ba1b9cfd195c7648a041ac500b072b419dd83
ErickAxelMartinezRios <91035271+ErickAxelMartinezRios@users.noreply.github.com>
2025-08-11 19:22:37 -0600

    Update app_py.py

--- app_py.py
+++ app_py.py
@@ -12,9 +12,9 @@ def summarize_text(text, hf_token):
     client = InferenceClient(model="facebook/bart-large-cnn", token=hf_token)
     response = client.summarization(text[:1000])
     st.write("Raw response:", response)  # <-- Add this to debug
-    if isinstance(response, list) and len(response) > 0:
+    if len(response) > 0:
         return response[0].get("summary_text", "No summary returned.")
-    return "No summary returned."
+    #return "No summary returned."
 
 
 st.title("📄 Technical Paper Summarizer")
